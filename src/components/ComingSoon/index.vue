c3fbb22b078d70b666a57c747c4584a157a12d22
unknown <929822043@qq.com>
2020-03-24 15:24:44 +0800

    即将上映页面下拉提示

--- src/components/ComingSoon/index.vue
+++ src/components/ComingSoon/index.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="movie_body">
         <Loading v-if="isLoading" />
-        <Scroller v-else>
+        <Scroller v-else :handleToScroll="handleToScroll" :handleToTouchEnd="handleToTouchEnd">
             <ul>
+                <li class="pullDown">{{pullDownMsg}}</li>
                 <li v-for="item in comingList" :key="item.id">
                     <div class="pic_show" @tap="handleToDetail(item.id)"><img :src="item.img | replaceWh('128.180')"></div>
                     <div class="info_list">
@@ -27,7 +28,8 @@ export default {
         return {
             comingList :   [],
             isLoading : true,
-            prevCityId : -1
+            prevCityId : -1,
+            pullDownMsg:''
 
         }
     },
@@ -44,8 +46,27 @@ export default {
     methods:{
         handleToDetail(movieId){
             this.$router.push('/movie/detail/2/' + movieId);
+        },
+        handleToScroll(pos){
+            if (pos.y > 30) {
+                this.pullDownMsg = '正在更新....'
+            }
+
+        },
+        handleToTouchEnd(pos){
+            if (pos.y > 30) {
+                this.axios.get('/api/movieOnInfoList?cityId=10').then((res)=>{
+                    var msg = res.data.msg;
+                    if (msg === 'ok') {
+                        this.pullDownMsg = '更新成功';
+                        setTimeout(() => {
+                            this.movieList = res.data.data.movieList;
+                            this.pullDownMsg = '';
+                        }, 1000);
+                    }
+                })
+            }
         }
-        
     }
 }
 </script>
@@ -63,4 +84,5 @@ export default {
 .movie_body .info_list img{ width:50px; position: absolute; right:10px; top: 5px;}
 .movie_body .btn_mall , .movie_body .btn_pre{ width:47px; height:27px; line-height: 28px; text-align: center; background-color: #f03d37; color: #fff; border-radius: 4px; font-size: 12px; cursor: pointer;}
 .movie_body .btn_pre{ background-color: #3c9fe6;}
+.movie_body .pullDown{ margin:0; padding:0; border:none;justify-content: center;}
 </style>
