3531537bf4a07da35a668e8419e30334fa8ed616
zipengxizo <929822043@qq.com>
2020-06-08 13:52:35 +0800

    add  less loader

--- src/view/Movies/detail.vue
+++ src/view/Movies/detail.vue
@@ -58,6 +58,7 @@ import "echarts/lib/chart/line";
 import "echarts/lib/component/tooltip";
 import "echarts/lib/component/title";
 import "echarts/lib/component/toolbox";
+import "./detail.less"
 
 var stats = [
   { label: "记忆力", value: 100 },
@@ -175,109 +176,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-#detailContrainer {
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: 100;
-  width: 100%;
-  min-height: 100%;
-  background: white;
-}
-#detailContrainer.slide-enter-active {
-  animation: 0.3s slideMove;
-}
-@keyframes slideMove {
-  0% {
-    transform: translateX(100%);
-  }
-  100% {
-    transform: translateX(0);
-  }
-}
-#content.contentDetail {
-  display: block;
-  margin-bottom: 0;
-}
-#content .detail_list {
-  height: 200px;
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-}
-.detail_list .detail_list_bg {
-  width: 100%;
-  height: 100%;
-  background: 0 40%;
-  filter: blur(20px);
-  background-size: cover;
-  position: absolute;
-  left: 0;
-  top: 0;
-}
-.detail_list .detail_list_filter {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background-color: #40454d;
-  opacity: 0.55;
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: 1;
-}
-.detail_list .detail_list_content {
-  display: flex;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  left: 0;
-  top: 0;
-  z-index: 2;
-}
-.detail_list .detail_list_img {
-  width: 108px;
-  height: 150px;
-  border: solid 1px #f0f2f3;
-  margin: 20px;
-}
-.detail_list .detail_list_img img {
-  width: 100%;
-  height: 100%;
-}
-.detail_list .detail_list_info {
-  margin-top: 20px;
-}
-.detail_list .detail_list_info h2 {
-  font-size: 20px;
-  color: white;
-  font-weight: normal;
-  line-height: 40px;
-}
-.detail_list .detail_list_info p {
-  color: white;
-  line-height: 20px;
-  font-size: 14px;
-  color: #ccc;
-}
-#content .detail_intro {
-  padding: 10px;
-}
-#content .detail_player {
-  margin: 20px;
-}
-.detail_player .swiper-slide {
-  width: 70px;
-  margin-right: 20px;
-  text-align: center;
-  font-size: 14px;
-}
-.detail_player .swiper-slide img {
-  width: 100%;
-  margin-bottom: 5px;
-}
-.detail_player .swiper-slide p:nth-of-type(2) {
-  color: #999;
-}
-</style>
--- src/view/Movies/index.vue
+++ src/view/Movies/index.vue
@@ -1,81 +1,137 @@
 <template>
-      <div id="main">
-        <myheader title="电影" />
-            <div id="content">
-                <div class="movie_menu">
-
-                    <router-link tag="div" to="/movie/city" class="city_name">
-                        <span>{{$store.state.city.cityName}}</span><i class="iconfont icon-lower-triangle"></i>
-                    </router-link>
-                    <div class="hot_swtich">
-                        <router-link tag="div" to="/movie/nowPlaying" class="hot_item">正在上映</router-link>
-                        <router-link tag="div" to="/movie/comingSoon" class="hot_item">即将上映</router-link>
-                    </div>
-                    <router-link tag="div" to="/movie/search" class="search_entry">
-                        <i class="iconfont icon-sousuo"></i>
-                    </router-link>
-                    
-                </div>
-                <keep-alive>
-                    <router-view />
-                </keep-alive>
-            </div>
-        <mytabbar />
-        <router-view name="detail" />
+  <div id="main">
+    <myheader title="电影" />
+    <div id="content">
+      <div class="movie_menu">
+        <router-link tag="div" to="/movie/city" class="city_name">
+          <span>{{$store.state.city.cityName}}</span>
+          <i class="iconfont icon-lower-triangle"></i>
+        </router-link>
+        <div class="hot_swtich">
+          <router-link tag="div" to="/movie/nowPlaying" class="hot_item">正在上映</router-link>
+          <router-link tag="div" to="/movie/comingSoon" class="hot_item">即将上映</router-link>
+        </div>
+        <router-link tag="div" to="/movie/search" class="search_entry">
+          <i class="iconfont icon-sousuo"></i>
+        </router-link>
+      </div>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
     </div>
+    <mytabbar />
+    <router-view name="detail" />
+  </div>
 </template>
 
 <script>
-
 // import Header from '@/components/Header';
 // import TabBar from '@/components/TabBar';
-import {messageBox} from '@/components/JS/Alert';
+import { messageBox } from "@/components/JS/Alert";
 
 export default {
-  name:'movie',
-  components:{
+  name: "movie",
+  components: {
     // Header,
     // TabBar
   },
   mounted() {
-    setTimeout(()=>{
-        this.$api.location.location().then((res) => {
-            var msg = res.data.msg;
-            if(msg === 'ok'){
-                let {nm,id} = res.data.data;
-                if( this.$store.state.city.cityId == id ){return;}
-                messageBox({
-                    title : '定位',
-                    content : nm,
-                    cancel : '取消',
-                    ok : '切换定位',
-                    handleOk(){
-                        window.localStorage.setItem('nowNm',nm);
-                        window.localStorage.setItem('nowId',id);
-                        window.location.reload();
-                    }
-                });
+    setTimeout(() => {
+      this.$api.location
+        .location()
+        .then(res => {
+          var msg = res.data.msg;
+          if (msg === "ok") {
+            let { nm, id } = res.data.data;
+            if (this.$store.state.city.cityId == id) {
+              return;
             }
-        }).catch((err) => {
-            console.log(err);
+            messageBox({
+              title: "定位",
+              content: nm,
+              cancel: "取消",
+              ok: "切换定位",
+              handleOk() {
+                window.localStorage.setItem("nowNm", nm);
+                window.localStorage.setItem("nowId", id);
+                window.location.reload();
+              }
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
         });
-    },3000);
+    }, 3000);
   }
-
-}
+};
 </script>
 
-<style scoped>
-#content .movie_menu{ width: 100%; height: 45px; border-bottom:1px solid #e6e6e6; display: flex; justify-content:space-between; align-items:center; background:white; z-index:10;}
-.movie_menu .city_name{ margin-left: 20px; height:100%; line-height: 45px;overflow: hidden;}
-.movie_menu .city_name.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
-.movie_menu .city_name.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
-.movie_menu .hot_swtich{ display: flex;flex: 1; height:100%; line-height: 45px;}
-.movie_menu .hot_item{flex: 1; font-size: 15px; color:#666; width:80px; text-align:center; margin:0 12px; font-weight:700;}
-.movie_menu .hot_item.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
-.movie_menu .hot_item.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
-.movie_menu .search_entry{ margin-right:20px; height:100%; line-height: 45px;}
-.movie_menu .search_entry.active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
-.movie_menu .search_entry.router-link-active{ color: #ef4238; border-bottom: 2px #ef4238 solid;}
-.movie_menu .search_entry i{  font-size:24px; color:red;}
+<style lang="less" scoped>
+#content .movie_menu {
+  width: 100%;
+  height: 45px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: white;
+  z-index: 10;
+}
+.movie_menu .city_name {
+  margin-left: 20px;
+  height: 100%;
+  line-height: 45px;
+  overflow: hidden;
+}
+.movie_menu .city_name.active {
+  color: #ef4238;
+  border-bottom: 2px #ef4238 solid;
+}
+.movie_menu .city_name.router-link-active {
+  color: #ef4238;
+  border-bottom: 2px #ef4238 solid;
+}
+.movie_menu .hot_swtich {
+  display: flex;
+  flex: 1;
+  height: 100%;
+  line-height: 45px;
+}
+.movie_menu {
+  .hot_item {
+    flex: 1;
+    font-size: 15px;
+    color: #666;
+    width: 80px;
+    text-align: center;
+    margin: 0 12px;
+    font-weight: 700;
+  }
+}
+.movie_menu .hot_item.active {
+  color: #ef4238;
+  border-bottom: 2px #ef4238 solid;
+}
+.movie_menu .hot_item.router-link-active {
+  color: #ef4238;
+  border-bottom: 2px #ef4238 solid;
+}
+.movie_menu .search_entry {
+  margin-right: 20px;
+  height: 100%;
+  line-height: 45px;
+}
+.movie_menu .search_entry.active {
+  color: #ef4238;
+  border-bottom: 2px #ef4238 solid;
+}
+.movie_menu .search_entry.router-link-active {
+  color: #ef4238;
+  border-bottom: 2px #ef4238 solid;
+}
+.movie_menu .search_entry i {
+  font-size: 24px;
+  color: red;
+}
 </style>
